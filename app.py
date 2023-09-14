ac14cdd9b157c559c9ddb5dfb2abe263704fe93d
alibaba0507 <alibaba0507@gmail.com>
2023-09-14 13:36:41 +0300

    init

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from plot_generator import generate_plot,generate_supp_ress_plot  # Import the generate_plot function
-
+from lstm_model_gen import generate_lstm_plot
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,14 +13,24 @@ def generate_plot_route():
     periods = request.form.getlist('period')
     shift = request.form.getlist('shift-backwards-period')
     loop = request.form.getlist('offset-times')
+    model_type= request.form.getlist('model-select')
     #print(shift)
     #print(loop)
+    merged_list = []
     # Call the generate_plot function with user selections as parameters
-    plot_data = generate_plot(currency_pairs, periods,shift,loop)
-    plot_lines = generate_supp_ress_plot(currency_pairs)
-    merged_list = plot_lines + plot_data
+    if model_type[0] == 'SGD' or model_type[0] == 'SGD_SR':
+        plot_data = generate_plot(currency_pairs, periods,shift,loop)
+        merged_list = plot_data
+    if model_type[0] == 'SGD_SR':
+        plot_lines = generate_supp_ress_plot(currency_pairs)
+        merged_list = plot_lines + plot_data
+    if model_type[0] == 'LSTM': 
+        lstm_plot = generate_lstm_plot(currency_pairs, periods)
+        merged_list = lstm_plot #plot_lines + plot_data
+    
     # Return the plot data as JSON
     return jsonify(merged_list)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run()
